01bf93488ca62dba7fc01b84bd02358c6bdfdd76
Jan Weidner <jw3126@gmail.com>
2017-08-04 10:53:54 +0200

    basic lambdas

--- src/Eval.hs
+++ src/Eval.hs
@@ -3,8 +3,7 @@ module Eval(
 ) where
 
 import Expr
-
-type EvalResult = Either String Ex
+import qualified Data.HashMap.Strict as HashMap
 
 evalNotImplementedError :: Ex -> EvalResult
 evalNotImplementedError x = Left $ "Eval on " ++ (show x) ++ " not yet implemented"
@@ -14,24 +13,38 @@ typeError x = Left $ "Type error evaluting " ++ (show x)
 
 undefVarError x = Left $ "Undefinded variable " ++ (show x)
 
-buildInFunctions = map ExAtom [
+builtin = map ExAtom [
     "+"
     , "-"
     , "*"
+    , "set"
+    , "fn"
     ]
-
 
 eval :: Ex -> EvalResult
 eval x@(ExInteger _) = Right x
 eval x@(ExBool _) = Right x
 eval x@(ExString _) = Right x
-eval x@(ExAtom _) = if x `elem` buildInFunctions then Right x else undefVarError x
-eval x@(ExList items) = let
+eval x@(ExFunction _ _) = Right x
+eval x@(ExAtom _) = if x `elem` builtin then Right x else undefVarError x
+eval x@(ExList (h:t)) = if h `elem` builtin then evalListBuiltin x else evalList x
+eval x@(ExList []) = evalNotImplementedError x
+eval x = evalNotImplementedError x
+
+evalList :: Ex -> EvalResult
+evalList x@(ExList items) = let
     a :: Either String [Ex]
     a = sequence $ map eval items
     in case a of
         Left msg -> Left msg
-        Right args -> eval1 $ ExList args
+        Right fargs -> eval1 $ ExList fargs
+evalList x = error $ "evalList on " ++ (show x)
+
+evalListBuiltin :: Ex -> EvalResult
+evalListBuiltin (ExList ((ExAtom "fn"):(ExList args):[body])) = Right $ ExFunction args body
+evalListBuiltin x@(ExList ((ExAtom "set"):(ExAtom key):[val])) = evalNotImplementedError x
+evalListBuiltin x@(ExList _) = evalList x
+evalListBuiltin x = error $ "evalListBuiltin on " ++ (show x)
 
 -- non recursive evaluation
 -- over defined arguments
@@ -45,6 +58,7 @@ eval1HeadTail (ExAtom "quote") t = Right $ ExList t
 eval1HeadTail (ExAtom "+") t = evalOp (+) 0 t
 eval1HeadTail (ExAtom "*") t = evalOp (*) 1 t
 eval1HeadTail (ExAtom "-") t = evalOp (-) 0 t
+eval1HeadTail f@(ExFunction _ _) args = eval1Function f args
 eval1HeadTail h t = evalNotImplementedError $ ExList (h:t)
 
 liftOpIntInt :: (Integer -> Integer -> Integer) -> (EvalResult -> Ex -> EvalResult)
@@ -59,4 +73,19 @@ _liftOpIntInt op a b = error("TODO")
 
 evalOp :: (Integer -> Integer -> Integer) -> Integer -> [Ex] -> EvalResult
 evalOp op v0 args = foldl (liftOpIntInt op) (Right $ ExInteger v0) args
+
+eval1Function :: Ex -> [Ex] -> EvalResult
+eval1Function (ExFunction sig body) args = if length(sig) == (length(args)) then
+        let 
+            m = HashMap.fromList $ zip sig args
+        in
+            eval $ substitute m body
+    else
+        Left $ "Wrong number of arguments " ++ (show sig) ++ " " ++ (show args)
+eval1Function f args  = error $ "Not a function " ++ (show f)
+
+-- TODO quote and functions...
+substitute :: (HashMap.HashMap Ex Ex) -> Ex -> Ex
+substitute m ex@(ExList items) = ExList $ map (substitute m) items
+substitute m ex = HashMap.lookupDefault ex ex m
 
--- src/Expr.hs
+++ src/Expr.hs
@@ -1,17 +1,31 @@
 module Expr
     (
-     Ex(ExInteger, ExList, ExAtom, ExBool, ExString)
+     Ex(ExInteger, ExList, ExAtom, ExBool, ExString, ExFunction)
+    , EvalResult
     , toString
     , falseString, trueString
     ) where
 
 import Data.List
+import Data.Hashable
+
+type EvalResult = Either String Ex
 
 data Ex = ExInteger Integer 
     | ExList [Ex] 
     | ExAtom String 
     | ExString String
+    | ExFunction [Ex] Ex
     | ExBool Bool deriving (Show, Eq)
+
+-- TODO is there an automatic way to do this?
+instance Hashable Ex where
+    hashWithSalt salt (ExList inner) = hashWithSalt salt inner
+    hashWithSalt salt (ExAtom inner) = hashWithSalt salt inner
+    hashWithSalt salt (ExString inner) = hashWithSalt salt inner
+    hashWithSalt salt (ExBool inner) = hashWithSalt salt inner
+    hashWithSalt salt (ExFunction args body) = 
+        hashWithSalt (hashWithSalt salt args) body
 
 falseString = "#f"
 trueString = "#t"
@@ -23,3 +37,4 @@ toString (ExBool False) = falseString
 toString (ExBool True) = trueString
 toString (ExString s) = "\"" ++ s ++ "\""
 toString (ExList items) = "(" ++ (intercalate " " $ map toString items) ++ ")"
+toString (ExFunction args body) = toString $ ExList $ [ExAtom "fn", ExList args, body]
